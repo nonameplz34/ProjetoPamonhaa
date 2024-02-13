e2620a8341b71e1828a175cbff67b53b83d76485
nonameplz34 <minhavez52@gmail.com>
2024-02-13 08:45:05 -0300

    tentativaPamonha

---
+++ escript/index.js
@@ -0,0 +1,11 @@
+function contermais(){
+    document.getElementsByClassName("info")[0].style.backgroundColor = "lightPink";
+    
+}
+
+function contermenos(){
+    document.getElementsByClassName("info")[0]
+    .style.backgroundColor= "#9B6647";
+    
+}
+
